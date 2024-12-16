19d534ced143f402491d49328cdfcb60a579d6d0
sri-dhar <sridhartuli@gmail.com>
2024-12-16 14:44:21 +0530

    adding caching proxy

---
+++ go-caching-proxy/go.mod
@@ -0,0 +1,3 @@
+module github.com/sri-dhar/goLang/go-caching-proxy
+
+go 1.23.4
---
+++ go-caching-proxy/main.go
@@ -0,0 +1,11 @@
+package main
+
+import(
+  "fmt"
+  "github.com/sri-dha/GoLang/go-caching-proxy/cmd"
+)
+
+func main(){
+  fmt.Println("Executing main function")
+  // cmd.Execute()
+}
